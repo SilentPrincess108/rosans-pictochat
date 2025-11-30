fcd83091b85629c0b28bf6ef4398d49b81492d4d
Rosan <rosan.syal@gmail.com>
2025-11-29 23:09:55 -0500

    started learning js

---
+++ script.js
@@ -0,0 +1,8 @@
+//Learning JS!
+
+alert("Hello World!");
+alert("Welcome to my little website!");
+alert("Continue to see me yap abt my fav games!");
+//document.body.style.background = "red";
+console.log("Hello, World!");
+console.log("My name is Rosan!");
