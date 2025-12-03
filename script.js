c52408eacdc2bcb26ca21f5965d18ab132b6c309
Rosan <rosan.syal@gmail.com>
2025-12-02 22:23:36 -0500

    created game catalogue page and also played around wioth layout

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 //Learning JS!
 
-alert("Hello World!");
-alert("Welcome to my little website!");
-alert("Continue to see me yap abt my fav games!");
+//alert("Hello World!");
+//alert("Welcome to my little website!");
+//alert("Continue to see me yap abt my fav games!");
+
 //document.body.style.background = "red";
 console.log("Hello, World!");
 console.log("My name is Rosan!");
