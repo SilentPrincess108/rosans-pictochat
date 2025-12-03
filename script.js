bc4680e7379bd6d84611db77e13d62639c71b03c
Rosan <rosan.syal@gmail.com>
2025-12-02 23:42:44 -0500

    learning more js!

--- script.js
+++ script.js
@@ -7,3 +7,14 @@
 //document.body.style.background = "red";
 console.log("Hello, World!");
 console.log("My name is Rosan!");
+
+let game = "BOTW";
+console.log(game);
+
+game = "TOTK";
+console.log(game);
+
+console.dir(document.body);
+document.body.innerHTML = "<h1>Hello World!</h1>";
+document.body.style.color = "blue";
+document.body.style.backgroundColor = "yellow"
