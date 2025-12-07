463e0fec873925c2aa6e0263003d9449ca6e5514
Rosan <rosan.syal@gmail.com>
2025-12-07 14:39:52 -0500

    added dynamic selected game image

--- gamecat.js
+++ gamecat.js
@@ -1,9 +1,18 @@
 //Game Catalogue - Selecting a game
+const game_cover = document.getElementById("game-cover");
+const game_title = document.getElementById("game-title");
 const game = document.querySelectorAll(".game-item");
+let cover = null;
 
 game.forEach(item =>{
     item.addEventListener("click", () =>{
         //save selected game to local storage
         localStorage.setItem("selectedGame", item.id);
+    })
+
+    item.addEventListener("mouseover", () =>{
+        cover = item.id;
+        console.log(cover);
+        game_cover.setAttribute("src", "images/" + item.id + "_cover.jpg");
     })
 })
