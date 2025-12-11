91dcf5a1006ca9c47be77a50830e42202d0ac4b6
Rosan <rosan.syal@gmail.com>
2025-12-10 21:26:02 -0500

    added date

---
+++ index.js
@@ -0,0 +1,10 @@
+let date = new Date();
+
+dateFull = document.getElementById("day");
+time = document.getElementById("time");
+
+days = ["Sun.", "Mon.", "Tues.", "Wed.", "Thurs.", "Fri."];
+months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+dateFull.innerText = days[date.getDay()] + " " + months[date.getMonth()] + " " + date.getDate() + ", " + date.getFullYear();
+time.innerText = date.getHours() + ":" + date.getMinutes();
