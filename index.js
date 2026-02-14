2c0c90f221abca4c54214bc4f789c4aa7b08af72
Rosan <rosan.syal@gmail.com>
2026-02-14 13:22:13 -0500

    added majora;s mask to game catalogue

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ let date = new Date();
 dateFull = document.getElementById("day");
 time = document.getElementById("time");
 
-days = ["Sun.", "Mon.", "Tues.", "Wed.", "Thurs.", "Fri."];
+days = ["Sun.", "Mon.", "Tues.", "Wed.", "Thurs.", "Fri.", "Sat."];
 months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 
 dateFull.innerText = days[date.getDay()] + " " + months[date.getMonth()] + " " + date.getDate() + ", " + date.getFullYear();
