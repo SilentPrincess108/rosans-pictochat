4f350e6ac5aa11b57fccb5531aba65dfa3165728
Rosan <rosan.syal@gmail.com>
2026-02-18 13:02:21 -0500

    mostly finished design page

--- design.js
+++ design.js
@@ -1,3 +1,20 @@
+//Import the THREE.js library
+//import * as THREE from "https://cdn.skypack.dev/three@0.129.0/build/three.module.js";
+// To allow for the camera to move around the scene
+//import { OrbitControls } from "https://cdn.skypack.dev/three@0.129.0/examples/jsm/controls/OrbitControls.js";
+// To allow for importing the .gltf file
+//import { GLTFLoader } from "https://cdn.skypack.dev/three@0.129.0/examples/jsm/loaders/GLTFLoader.js";
+
+//create scene
+//const scene = new THREE.Scene();
+//create new camera with positions and angles
+//const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+//let object;
+//let control;
+//let objToRender = 'model';
+
+//vars for text
 const model_name = document.getElementById("name");
 const program = document.getElementById("program");
 const use = document.getElementById("use");
